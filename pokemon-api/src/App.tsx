b6259361e95ed016bbb511bec365f083332b5ea9
JOEL QUINTANA <joel@joelquintana.com>
2023-03-31 04:18:54 -0700

    Cleaned code with auto formatter using prettier

--- pokemon-api/src/App.tsx
+++ pokemon-api/src/App.tsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { fetchPokemonList, fetchPokemonData, PokemonData } from './api';
-import {useState, useEffect} from 'react';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { fetchPokemonList, fetchPokemonData, PokemonData } from "./api";
+import { useState, useEffect } from "react";
 
 function App() {
   const [pokemonList, setPokemonList] = useState<any[]>([]);
   const [pokemonData, setPokemonData] = useState<PokemonData[]>([]);
 
   useEffect(() => {
-
     fetchPokemonList().then((response) => {
       setPokemonList(response.results);
-    }
-    
-  );
-  },[]);
+    });
+  }, []);
 
-  useEffect(()=>{
-    let newPokemonData: PokemonData[] = [];
-    Promise.all(pokemonList.map((pokemon:any) => {
-      const pokemon_id = pokemon.url.split('/').slice(-2, -1)[0];
-      return fetchPokemonData(pokemon_id)
-    })).then((responses) => {
+  useEffect(() => {
+    Promise.all(
+      pokemonList.map((pokemon: any) => {
+        const pokemon_id = pokemon.url.split("/").slice(-2, -1)[0];
+        return fetchPokemonData(pokemon_id);
+      })
+    ).then((responses) => {
       setPokemonData(responses);
-    }
-    );
-  },[pokemonList]);
+    });
+  }, [pokemonList]);
 
   return (
     <div className="App">
@@ -45,28 +42,34 @@ function App() {
           Learn React
         </a>
         <div>
-        <table>
-              <thead>
-                <tr>
+          <table>
+            <thead>
+              <tr>
                 <td>ID</td>
                 <td>Name</td>
                 <td>Sprite</td>
-                </tr>
-              </thead>
-              <tbody>
-                {pokemonData.map((pokemon) => {
-
-            return <tr id={pokemon.id.toString()}>
-              <td>{pokemon.id}</td>
-                <td>{pokemon.name}</td>
-                  <td><img src={pokemon.sprites.front_default} alt={pokemon.name} height={"200px"} width={"200px"}/></td>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemonData.map((pokemon) => {
+                return (
+                  <tr id={pokemon.id.toString()}>
+                    <td>{pokemon.id}</td>
+                    <td>{pokemon.name}</td>
+                    <td>
+                      <img
+                        src={pokemon.sprites.front_default}
+                        alt={pokemon.name}
+                        height={"200px"}
+                        width={"200px"}
+                      />
+                    </td>
                   </tr>
-                  
-          })}
-          </tbody>
-                </table>
-
-          </div>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   );
--- pokemon-api/src/api.ts
+++ pokemon-api/src/api.ts
@@ -1,48 +1,41 @@
-const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
 export interface PokemonData {
-    id: number;
+  id: number;
   name: string;
   weight: number;
   sprites: {
     front_default: string;
-    };
+  };
 }
 
 export function fetchPokemonList(): Promise<any> {
   return fetch(API_URL)
     .then((response) => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        return response.json()
-    })
-    // .then((data) => {
-    //   return {
-    //     count: data.count,
-    //     results: data.results,
-    //   };
-    // })
-    .catch((error) => {
-        console.error('There was a problem with the network request:', error);
-        throw error;
-      });
-}
-
-export function fetchPokemonData(id: number): Promise<PokemonData> {
-    const fetch_data_uri = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-  //  console.log(fetch_data_uri);
-  return fetch(fetch_data_uri)
-    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       return response.json();
     })
     .catch((error) => {
-      console.error('There was a problem with the network request:', error);
+      console.error("There was a problem with the network request:", error);
+      throw error;
+    });
+}
+
+export function fetchPokemonData(id: number): Promise<PokemonData> {
+  const fetch_data_uri = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+  return fetch(fetch_data_uri)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("There was a problem with the network request:", error);
       throw error;
     });
 }
